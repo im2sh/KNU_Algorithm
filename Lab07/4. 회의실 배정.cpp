d3b14cc6d4ef46d72b93f23876ef12e788769296
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-24 00:27:37 +0900

    회의실

--- Lab07/4. 회의실 배정.cpp
+++ Lab07/4. 회의실 배정.cpp
@@ -5,18 +5,20 @@
 using namespace std;
 
 vector<pair<int, int>> v;
-vector<pair<int, int>> tot;
+vector<pair<int, int>> result;
 int N;
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
     return a.second < b.second;
 }
 
-int main() {
+void FastIO() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
 
+void Init() {
     cin >> N;
 
     for (int i = 0; i < N; i++) {
@@ -26,21 +28,30 @@ int main() {
     }
 
     sort(v.begin(), v.end(), cmp);
+}
 
+void solve() {
     int ret = 1;
     int end_T = v[0].second;
-    tot.push_back({v[0].first, v[0].second});
+    result.push_back({v[0].first, v[0].second});
 
     for (int i = 1; i < N; i++) {
         if (v[i].first >= end_T) {
             end_T = v[i].second;
             ret += 1;
-            tot.push_back({v[i].first, v[i].second});
+            result.push_back({v[i].first, v[i].second});
         }
     }
 
     cout << ret << "\n";
-    for (pair<int, int> temp: tot) {
+    for (pair<int, int> temp: result) {
         cout << temp.first << " " << temp.second << "\n";
     }
+}
+
+int main() {
+    FastIO();
+    Init();
+    solve();
+    return 0;
 }
